879d7b08d06ef0dbdbc7c27ce910a9b9475c9c06
laboratorio <laboratorio@UPOAULA10714>
2023-10-16 08:52:40 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MauroSalguero_EjercicioCam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
